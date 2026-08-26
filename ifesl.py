45bee7035bb5adfaffb1b0703cc0cd5059d9c578
victor hugo <victor.costa.hugo.2.vh@gmail.com>
2026-08-25 22:53:45 -0300

    Add files via upload

---
+++ ifesl.py
@@ -0,0 +1,35 @@
+# if = do some code only if some condition is true
+#     else do something else
+
+# age = int(input('enter your age: '))
+
+# if age >=100:
+#     print('you are too old to sign up!')
+# elif age>=18:
+#     print('you are now sined up!')
+# elif age < 0:
+#     print('you havent born yet!')
+# else:
+#     print('you must be 18+ to sign up!')
+
+# response = input('what do you would like food? (y/n): ')
+
+# if response == 'y':
+#     print('have some food!')
+# else:
+#     print('no food for you!')
+
+# name = input('enter your name: ')
+
+# if name == "":
+#     print('you did not type your name!')
+# else:
+#     print(f'hello {name}!')
+
+# for_sale = False
+
+# if for_sale:
+#     print('this item is for sale!')
+# else:
+#     print('this item is not for sale!') 
+
