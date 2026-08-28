3006b55580e020ba6660e09ceb17ea522790d4c5
victor hugo <victor.costa.hugo.2.vh@gmail.com>
2026-08-28 19:28:58 -0300

    Add files via upload

---
+++ slot_machine.py
@@ -0,0 +1,88 @@
+# Python slot machine 
+
+import random
+
+
+def spin_row():
+    symbols = ['🍒', '🍉', '🍋', '🔔', '⭐']
+
+    return [random.choice(symbols) for _ in range(3)]
+
+    # result = []
+    # for symblo in range(3):
+    #     result.append(random.choice(symbols))
+    # return results
+
+def print_row(row):
+    print("-------------")
+    print(" | ".join(row))
+    print("-------------")
+
+def get_payout(row, bet):
+    if row[0] == row[1] == row[2]:
+        if row[0] == '🍒':
+            return bet * 3
+        elif row[0] == '🍉':
+            return bet * 4
+        elif row[0] == '🍋':
+            return bet * 5
+        elif row[0] == '🔔':
+            return bet * 10
+        elif row[0] == '⭐':
+            return bet * 20
+    return 0
+
+def main():
+    balance = 100
+
+    print("**************************")
+    print("Welcome to Python slotes ")
+    print("Symbols: 🍒 🍉 🍋 🔔 ⭐")
+    print("**************************")
+
+    while balance > 0:
+        print(f"Current balance: ${balance}")
+
+        bet = input("Place your bet amount: ")
+
+        if not bet.isdigit():
+            print("Please enter a valid number")
+            continue
+
+        bet = int(bet)
+
+        if bet > balance:
+            print("Insufficient funds")
+            continue
+
+        if bet <= 0:
+            print("Bet must be greater than 0")
+            continue
+
+        balance -= bet
+
+        row = spin_row()
+        print("Spinning...\n")
+        print_row(row)
+
+        payout = get_payout(row, bet)
+
+        if payout > 0:
+            print(f"You won ${payout}")
+        else:
+            print("Sorry you lost this round")
+
+        balance += payout
+
+        play_again = input("Do you want to spin again (Y/N): ").upper()
+
+        if play_again != 'Y':
+            break
+
+    print("****************************************")
+    print(f"Game over! Your final balance ${balance}")
+    print("****************************************")
+
+
+if __name__ == '__main__':
+    main()
