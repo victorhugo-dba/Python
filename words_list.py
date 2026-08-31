cf830e202064b1da7e617773c00528785711cdb6
victor hugo <victor.costa.hugo.2.vh@gmail.com>
2026-08-31 20:31:36 -0300

    Add files via upload

---
+++ words_list.py
@@ -0,0 +1,7 @@
+# words for hangman game
+
+words = ("alligator", "ant", "aardvark", "bear", "bat", "beaver", "cat", "cow", "camel", "dog", "duck", "deer", "elephant", "eagle", "elk", "fish", "fox", "frog", "gorilla",
+         "giraffe", "goat", "horse", "hippo", "hyena", "iguana", "impala", "insect", "jellyfish", "jaguar", "jackal", "kangaroo", "koala", "kitten", "lion", "leopard", "llama",
+         "monkey", "moose", "mouse", "narwhal", "newt", "numbat", "octopus", "owl", "ostrich", "penguin", "pig", "panda", "quail", "quokka", "quoll", "raccoon", "rabbit",
+         "rhino", "snake", "shark", "sloth", "turtle", "tiger", "toad", "urchin", "uakari", "urial", "vulture", "viper", "vicuña", "walrus", "whale", "wolf", "xerus", "yak",
+         "zebra", "zebu", "zokor")
